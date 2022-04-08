99aebbf71a2409823391140bff154279bc999b47
Radu Potop <radu@wooptoo.com>
2022-04-08 14:34:09 +0100

    Added keyboard shortcut

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 'use strict'
 
 function tabHandler(tab) {
+    console.log('Handling Tab', tab)
     browser.tabs.sendMessage(tab.id, { action: 'getURL' })
 }
 
@@ -11,3 +12,10 @@ function downloadFile(params) {
 
 browser.pageAction.onClicked.addListener(tabHandler)
 browser.runtime.onMessage.addListener(downloadFile)
+
+browser.commands.onCommand.addListener(function (command) {
+    if (command === 'download-video') {
+        var activeTab = browser.tabs.query({ active: true, currentWindow: true })
+        activeTab.then((tabList) => tabHandler(tabList[0]))
+    }
+})
