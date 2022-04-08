dfa15205a144a679dafd6bd75d865957e2edbe34
Radu Potop <radu@wooptoo.com>
2022-04-08 01:02:52 +0100

    Initial version

---
+++ background.js
@@ -0,0 +1,11 @@
+function tabHandler(tab) {
+    browser.tabs.sendMessage(tab.id, {action: 'getURL'})
+}
+
+function downloadFile(params) {
+    console.log('Downloading:', params)
+    browser.downloads.download(params)
+}
+
+browser.pageAction.onClicked.addListener(tabHandler);
+browser.runtime.onMessage.addListener(downloadFile)
