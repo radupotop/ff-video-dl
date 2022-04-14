1db42c30a1f09a6f6b2f660ef5d6a0b923780ae8
Radu Potop <radu@wooptoo.com>
2022-04-14 23:51:19 +0100

    Conditionally download transcript

--- content.js
+++ content.js
@@ -13,7 +13,8 @@ function parseVideo(document) {
 }
 
 function parseTranscript(document) {
-    return document.getElementsByClassName('classroom-transcript')[0].innerText
+    let trs = document.getElementsByClassName('classroom-transcript')[0]
+    return trs ? trs.innerText : false
 }
 
 function getVideoUrl(msg) {
@@ -28,13 +29,15 @@ function getVideoUrl(msg) {
     let vid = parseVideo(document)
     let trs = parseTranscript(document)
     let vid_name = sanitiseTitle(_ts + '__' + document.title)
-    let trs_name = vid_name + '.txt'
     let video_params = { filename: vid_name, url: vid.src }
-    let trs_params = { action: 'build', filename: trs_name, contents: trs }
     console.log('Fetch video:', video_params)
     browser.runtime.sendMessage(video_params)
-    console.log('Fetch transcript:', trs_params)
-    browser.runtime.sendMessage(trs_params)
+    if (trs) {
+        let trs_name = vid_name + '.txt'
+        let trs_params = { action: 'build', filename: trs_name, contents: trs }
+        console.log('Fetch transcript:', trs_params)
+        browser.runtime.sendMessage(trs_params)
+    }
 }
 
 browser.runtime.onMessage.addListener(getVideoUrl)
