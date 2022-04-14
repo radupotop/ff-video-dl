6e6e552d12203790e5c26eeb377fd1e10056d943
Radu Potop <radu@wooptoo.com>
2022-04-15 00:03:08 +0100

    Some improvements to file names

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const TRS_CLASS = 'classroom-transcript'
+
 function hasVideo() {
     return !!document.getElementsByTagName('video').length
 }
@@ -13,7 +15,7 @@ function parseVideo(document) {
 }
 
 function parseTranscript(document) {
-    let trs = document.getElementsByClassName('classroom-transcript')[0]
+    let trs = document.getElementsByClassName(TRS_CLASS)[0]
     return trs ? trs.innerText : false
 }
 
@@ -26,14 +28,15 @@ function getVideoUrl(msg) {
         return
     }
     let _ts = Date.now()
+    let base_name = sanitiseTitle(_ts + '__' + document.title)
     let vid = parseVideo(document)
     let trs = parseTranscript(document)
-    let vid_name = sanitiseTitle(_ts + '__' + document.title)
+    let vid_name = base_name + '.mp4'
     let video_params = { filename: vid_name, url: vid.src }
     console.log('Fetch video:', video_params)
     browser.runtime.sendMessage(video_params)
     if (trs) {
-        let trs_name = vid_name + '.txt'
+        let trs_name = base_name + '.txt'
         let trs_params = { action: 'build', filename: trs_name, contents: trs }
         console.log('Fetch transcript:', trs_params)
         browser.runtime.sendMessage(trs_params)
